8b06e6c8f6ce7ceaf16afe1195240bd4908fa69c
Ayham Najjar <ayhamnajjar851@gmail.com>
2026-03-29 15:51:49 +0300

    Initial commit: React + Vite + Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./index.css"
+
+function App() {
+  return (
+   <div className="">
+      <h1 className="text-5xl font-bold text-blue-600">
+        Hello CallX
+      </h1>
+    </div>
+  );
+}
+export default App
